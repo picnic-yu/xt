6364f206c130cd04a586d6f295d7090dc97dbb28
unknown <164005206@qq.com>
2018-12-02 16:51:30 +0800

    实现简单的路由服务器

--- src/application.js
+++ src/application.js
@@ -1,36 +1,39 @@
 const http = require('http');
 const url = require('url');
-function createApplication(){
-    console.log(1)
-
-    //app其实就是真正的请求监听函数
-    let app = function (req,res){
-        console.log(2)
-        const {pathname} = url.parse(req.url,true)
-        for(let i =0;i<app.routes.length;i++){
-            let route = app.routes[i]
-            if(route.method == req.method.toLowerCase()
-                && route.path == pathname){
-                    return route.handler(req,res)
-            }
+let router =  [
+    {
+        path:'*',
+        method:'*',
+        handler(req,res){
+            res.end('not')
         }
-        res.end('not')
     }
-    app.get = function(path,handler){
-        console.log(3)
-        app.routes.push({
-            method:'get',
-            path,
-            handler
-        })
+]
+function createApplication(){ 
+    return {
+        get (path,handler){
+            console.log(3)
+            router.push({
+                method:'get',
+                path,
+                handler
+            })
+        },
+        listen (){
+            let server = http.createServer(function(req,res){
+                const {pathname} = url.parse(req.url,true)
+                for(let i =1;i<router.length;i++){
+                    let {path,method,handler} = router[i];
+                    if(pathname == path && method == req.method.toLowerCase() ){
+                        return handler(req,res);
+                    }
+                    
+                }
+                router[0].handler(req,res);
+            });
+            
+            server.listen.apply(server,arguments);
+        }
     }
-    app.routes = []
-    app.listen = function(){
-        console.log(4)
-        let server = http.createServer(app);
-        
-        server.listen.apply(server,arguments);
-    }
-    return app;
 }
 module.exports = createApplication;
